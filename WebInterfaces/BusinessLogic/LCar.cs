73c3ea81938df55a7473b5107fe7d5da14e35870
stargatesis <esolis.sis@gmail.com>
2024-12-30 11:59:13 -0600

    Proyecto Interfaces

---
+++ WebInterfaces/BusinessLogic/LCar.cs
@@ -0,0 +1,18 @@
+﻿using Data;
+using Data.Interfaces;
+
+namespace BusinessLogic
+{
+    public class LCar : ICar 
+    {
+        public bool addCar(string marca, string modelo)
+        {
+            return true;
+        }
+
+        public List<string> carList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebInterfaces/Data/Interfaces/ICar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interfaces
+{
+    public interface ICar
+    {
+       bool addCar(string marca, string modelo);
+
+        List<string> carList(); 
+
+    }
+}
